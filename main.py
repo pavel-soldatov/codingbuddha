5d9c5eb168c0318ba8abf4ca7aeb1e35691a63bd
pavel-soldatov <161460386+pavel-soldatov@users.noreply.github.com>
2024-07-10 09:01:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+logo = '''
+                                                   Welcome to the coding buddha
+
+                        _ooOoo_
+                       o8888888o
+                       88" . "88
+                       (| -_- |)
+                       O\  =  /O          All that we are is the result of what we have thought.
+                    ____/`---'\____       The mind is everything.
+                  .'  |\|     |//  `.     What we think we become.”
+                 /    |||  :  |||//  |                                     Buddha
+               /  _||||| -:- |||||_  |
+                |   | |\|  -  /'| |   |
+                | \_|  `\`---'//  |_/ |
+                \  .-\__ `-. -'__/-.  /                 Its time to code some shit up baby :)
+              ___`. .'  /--.--\  `. .'___
+           ."" '<  `.___\_<|>_/___.' _> |"".
+          | | :  `- \`. ;`. _/; .'/ /  .' ; |
+          \  \ `-.   \_\_`. _.'_/_/  -' _.' /
+===========`-.`___`-.__\ \___  /__.-'_.'_.-'================
+                        `=--=-'                       
+'''
+print(logo)
+def start_button():
+    while True:
+        inp = input("To start please type 'start' >> ")
+        if inp == 'start':
+            break
+
+start_button()
+
